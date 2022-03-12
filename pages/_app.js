07fda7018aae084bf4e5ce34b0d53bb4cc633de8
faezenasiri <faezenasiriii@gmail.com>
2022-03-12 15:12:40 +0330

    fi

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,33 @@
 import '../styles/globals.css'
-
+import Link from 'next/link'
 
 
 function Marketplace({ Component, pageProps }) {
   return (
     <div>
   
-          <nav className="border-b p-6">
+        <nav className="border-b p-6">
+ 
+        <div className="flex mt-4">
+  
+          <Link href="/buy">
+            <a className="mr-6 text-pink-500">
+              Buy
+            </a>
+          </Link>
+          <Link href="/create">
+            <a className="mr-6 text-pink-500">
+              Create
+            </a>
+          </Link>
+
+        </div>
+      </nav>
  
         
 
         
-      </nav>
+     
       <Component {...pageProps} />
     </div>
   )
